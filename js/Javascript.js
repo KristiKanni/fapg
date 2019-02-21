7bd8fb1f7e3c02cff3151cbd79bed1518e9bc2aa
fabretti <Glebmak@mail.ru>
2019-02-21 21:34:30 +0300

    Faq

---
+++ js/Javascript.js
@@ -0,0 +1,33 @@
+  var element = document.querySelectorAll("div");
+
+  console.log(element);
+  
+function () {
+	try 
+	{
+		var 01:SomeObjectType1 = New SomeObjectType1();
+		var C1:SomeChildType1 = 01.NewChild();
+		...	
+	}
+	catch(e)
+
+	{
+		try
+		{
+			show(e.message):
+		}
+		catch(e2)
+		{
+			throw(e2)
+		}
+		finally
+		{
+			01 = null;
+		}
+	}
+	finally
+	{
+		C1 = null;
+	}
+ console.log(e);
+}
